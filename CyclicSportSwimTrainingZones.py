60891a0962738124a98b7924cc540d1cc80c843c
UpDownSide <47486602+UpDownSide@users.noreply.github.com>
2020-04-15 21:45:33 +0100

    Update CyclicSportSwimTrainingZones.py

--- CyclicSportSwimTrainingZones.py
+++ CyclicSportSwimTrainingZones.py
@@ -1,7 +1,8 @@
 
+
 #This script serves to cyclic sport, it aims the limits of energy production
 #we know that these limits can be trained. Use a professional of this area 
-#when training, be carefull... 
+#when training, be carefull... You can use this piece a code for the best purposes
 
 import time
 
@@ -23,7 +24,7 @@ print (RecA0, 'm/s','is the speed for recovering purposes')
 FatA1 = d*0.6
 print (FatA1, 'm/s','is the speed for burning fat')
 GlycogenA2 = d*0.6 and d*0.8
-print (GlycogenA2, 'm/s','is the speed for increase glycolic metabolism and lactate threshold')
+print (GlycogenA2,'and', FatA1, 'm/s','are the limits of speed for increase glycolic metabolism and lactate threshold')
 AllA3 = d*0.95
 print (AllA3, 'm/s','is the speed for increase lactate tolerance and VO2Max')
 
@@ -33,15 +34,23 @@ print ('You need to complete that distance in',(round (T0)),'seconds','for recov
 T1 = b/FatA1
 print ('You need to complete that distance in',(round (T1)),'seconds','for burning fat')
 T2 = b/GlycogenA2
-print ('You need to complete that distance in',(round (T2)),'seconds', 'for increase glycolic metabolism and lactate threshold')
+print ('You need to complete that distance between',(round (T1)),'and',(round (T2)),'seconds', 'for increase glycolic metabolism and lactate threshold')
 T3 = b/AllA3
 print ('You need to complete that distance in',(round (T3)),'seconds','for increase lactate tolerance and VO2Max')
 
 time.sleep(3)
+
+print ('Well, VO2Max only at maximum speeds. At what speed you finish a race?')
+
+time.sleep(3)
+
 print('oVer and Over... Again ')
 
 time.sleep(2)
-print('Again')
+print('Again...')
+
+time.sleep(3)
+print('Do not miss the next chapter.... We dont')
  
 # BiBle - Workload and perception of effort in swimming training - Luís Rama, Ana M Teixeira
 # Among others
